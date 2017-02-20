7b11b451906dd3befcad4dea64b0fceb59fd58e3
lucas <lucas.sebastian.monzon@gmail.com>
2017-02-20 03:03:27 -0300

    vaa

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,11 +1,20 @@
 import express from 'express'
 const router = express.Router()
 
-import {Client, Person} from 'src/server/models'
+import {Client, Person ,Chofer} from 'src/server/models'
 
 // GET  /api/votes
 router.get('/client', (req, res) => {
-  Client.find({}, (err, docs) => {
+  Client.find({}).populate("_person").exec((err,docs) => {
+    if (err) {
+      return res.sendStatus(500).json(err)
+    }
+    res.json(docs)
+  })
+})
+
+router.get('/client/:codigo_client', (req, res) => {
+  Client.find({'codigo':req.params.codigo_client}).populate("_person").exec((err,docs) => {
     if (err) {
       return res.sendStatus(500).json(err)
     }
@@ -22,21 +31,57 @@ router.post('/client', (req, res) => {
           return res.sendStatus(500).json(err)
         }
         let client = new Client(
-          { _person : person._id,
-            codigo  : "un codigo"
+          { _person : person._id
 
           }
         );
-        client.populate("client._person")
-        res.json(client);
+        client.save(err=> Client.populate(client,{path:"_person"},(err,client)=>res.json(client)))
       }
     }
+
 
    let person = new Person()
    person.nombre = req.body.nombre
    person.apellido=req.body.apellido
-
+   person.tipoDoc=req.body.tipoDoc
+   person.numeroDoc=req.body.numeroDoc
+   person.celular=req.body.celular
+   person.telefono=req.body.telefono
+   person.calle=req.body.calle
+   person.numeroPuerta=req.body.numeroPuerta
    person.save(onSave(person))
+
+
+})
+
+// POST /api/vote/123
+router.post('/chofer', (req, res) => {
+
+    var onSave = function (person,nroLinc) {
+      return function (err) {
+        if (err) {
+          return res.sendStatus(500).json(err)
+        }
+        let chofer = new Chofer(
+          { _person : person._id,
+            nroLinc :nroLinc
+          }
+        );
+        chofer.save(err=> Chofer.populate(chofer,{path:"_person"},(err,chofer)=>res.json(chofer)))
+      }
+    }
+
+
+   let person = new Person()
+   person.nombre = req.body.nombre
+   person.apellido=req.body.apellido
+   person.tipoDoc=req.body.tipoDoc
+   person.numeroDoc=req.body.numeroDoc
+   person.celular=req.body.celular
+   person.telefono=req.body.telefono
+   person.calle=req.body.calle
+   person.numeroPuerta=req.body.numeroPuerta
+   person.save(onSave(person,req.body.nroLinc))
 
 
 })
--- src/server/models/index.js
+++ src/server/models/index.js
@@ -12,15 +12,53 @@ let PersonSchema = new mongoose.Schema({
 
 })
 
+let CounterSchema = new mongoose.Schema({
+  _id:{type: String,required:true},
+  codigo        :{type : String}
+})
+
+let Counter = mongoose.model('Counter', CounterSchema);
 
 let ClientSchema = new mongoose.Schema({
   _person       :{type: mongoose.Schema.Types.ObjectId, ref: 'Person'},
   codigo        :{type : String}
 })
 
+ClientSchema.pre('save', function(next) {
+
+  let doc = this;
+  Counter.findById('ClientID',(err, counter)=>{
+    if (err)
+    return   console.log(err);
+    if (counter) {
+      let increment = counter.codigo.slice(2);
+     increment = Number(increment)+1;
+      counter.codigo="RD"+increment ;
+
+      counter.save(err=> {
+        if (err)
+        return   console.log(err);
+
+        doc.codigo=counter.codigo;
+        next();
+      }
+      )
+    }else {
+          let  newc=new Counter();
+        newc.codigo='RD1';
+        newc._id="ClientID"
+
+        newc.save(err=>{next()})
+      }
+    });
+
+
+
+  });
+
 let ChoferSchema = new mongoose.Schema({
   _person       :{type: mongoose.Schema.Types.ObjectId, ref: 'Person'},
-  nroLinc   : Number
+  nroLinc       : Number
 
 })
 
